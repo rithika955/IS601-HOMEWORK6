4b3c66f46c986ba1a28e6380c831670970f02b61
rithika955 <rk955@njit.edu>
2024-10-17 14:53:23 -0400

    Update add_command.py

--- app/plugins/add_command.py
+++ app/plugins/add_command.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from calculator.commands import Command
+from app.commands import Command
 
 class AddCommand(Command):
     def execute(self, a: Decimal, b: Decimal) -> Decimal:
