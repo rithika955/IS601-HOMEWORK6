4833673334c8b9e43b4ef6c5e53050ef28dc240d
rithika955 <rk955@njit.edu>
2024-10-17 14:02:52 -0400

    Update __init__.py

--- app/plugins/greet/__init__.py
+++ app/plugins/greet/__init__.py
@@ -1,8 +1,10 @@
 import logging
 from app.commands import Command
-
+from icecream import ic
+from app import App
 
 class GreetCommand(Command):
     def execute(self):
         logging.info("Hello, World!")
+        logging.debug(ic(App))
         print("Hello, World!")
