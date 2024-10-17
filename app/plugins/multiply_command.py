c9e5884804c0cb00ebb49e8334808f1b16f8a0d5
rithika955 <rk955@njit.edu>
2024-10-17 14:54:02 -0400

    Update multiply_command.py

--- app/plugins/multiply_command.py
+++ app/plugins/multiply_command.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from calculator.commands import Command
+from app.commands import Command
 
 class MultiplyCommand(Command):
     def execute(self, a: Decimal, b: Decimal) -> Decimal:
